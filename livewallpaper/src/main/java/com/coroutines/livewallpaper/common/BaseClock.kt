de7d5863dc3e2756f897539542bc5103be28b6a2
shpinardmitri <dmitrinyc@yahoo.com>
2025-09-10 13:30:41 -0400

    wallpaperservice work in progress - custom horizontal pager

---
+++ livewallpaper/src/main/java/com/coroutines/livewallpaper/common/BaseClock.kt
@@ -0,0 +1,9 @@
+package com.coroutines.livewallpaper.common
+
+import android.graphics.Canvas
+
+interface BaseClock {
+    fun updateTextSizes(width: Int)
+    fun draw(canvas: Canvas)
+    fun destroy()
+}
