bfcb417534eff3faf284c0a4eba40626c3b3a6ae
shpinardmitri <dmitrinyc@yahoo.com>
2025-11-22 11:06:26 -0500

    wearos fixes

--- wearos/src/main/java/com/coroutines/swisstime/wearos/ui/AnimatedCard.kt
+++ wearos/src/main/java/com/coroutines/swisstime/wearos/ui/AnimatedCard.kt
@@ -13,10 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-//import androidx.compose.material.icons.filled.DirectionsWalk
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.PlayArrow
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
